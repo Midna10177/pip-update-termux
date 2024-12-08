22bfbd51206dd529cc903acadcb5d3b2007c0e3e
Renny <tim10177@yahoo.com>
2024-12-08 03:37:18 -0800

    Implement Process Substitution
    Reworked to not use any temporary files.

--- pipupdate.sh
+++ pipupdate.sh
@@ -1,17 +1,13 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/usr/bin/bash
 
 #python program that gets our out of date packages and writes to requirements.txt
-pythonProgram="H4sICPZB2GQCA3RtcHB5LnB5AG2QMU/DMBCFd/+KwwxNRJsM3ZA6wQoslRhKhdzk0riNfcY+K+2/x2lCKRKLdXrv7rt3vr8ra8VqfCoyBaM38VQ2usNQxuDLnbalO3NLdikaTwYogDaOPEM4B0YzqiHunKcKw9WtWqyOnxTZRRaTdghkhRjga3r3mhFWID1+Re3RoOVQ8ImlEDU2UNPTy3OWMhll6/xRgEeO3v7h/thzCC123WrtI+bj+B75VRkM2bDzbXfAim8gG72RNtlyCw150KAt/DZuhaBLEVK+QS46UnXIxkjSabeETgeGxSLlSH+HdSoTyCheDf0yz4VT1VHt8RIiYa55JnKK6by2nMmEAEr3JgxMQ+ERZD75M5gVB9I2uwUmvug1t0AObXbzoXPZyxxUgCYd2xT9oGWzD/s/Y0rQe0q7mUA+3KBy8Q16RLx7HgIAAA=="
+pythonProgram="H4sIAAAAAAAAA1WOMU/DMBCFd/+KIwxNJNoM3ZAywQosbKVCrn1pnMY+y3ce+u9xk1LKdnrv3qfv8aHNnNqDC208y0Bhq/pEHojB+UhJgM8s6JeU8yEmMsi31gxoTt+UJWZR12xkCkpZ7MHSy9trbch7HWzzrCCh5BT+rX7rJ+ABp6n7TBmbZX5Eedceub4QPw4jGrmD7NyuCqWu9tBTAgcuwN/jXimaD4ZujjcTacv1olRFF7cwORZYr4uH1YK2nAXktXSX/6ppVNTmpI84SxTMzedKLpoxuSD16iusNiO5UN8vCuAH8HkjIV8BAAA="
 
-pythonProgramFile="pip-get-requirements.py"
+#pushd ~
 
-pushd /data/data/com.termux/files/usr/tmp/
-echo $pythonProgram | base64 -d | gzip -dc > $pythonProgramFile
-chmod +x $pythonProgramFile
+#python3 -m venv .venv
+#source .venv/bin/activate
 
-./$pythonProgramFile
+python3 -m pip install -r <(python3 <(echo $pythonProgram | base64 -d | gzip -dc)) -U
 
-python3 -m pip install -r requirements.txt -U
-
-rm requirements.txt $pythonProgramFile
-popd
+#popd
